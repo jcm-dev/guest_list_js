fd32867967cb029b81d518c7e123034572f48473
Joshua <meyer.josh8@gmail.com>
2021-02-22 20:19:03 -0600

    Assign and set querySelector variables

---
+++ app.js
@@ -0,0 +1,6 @@
+// grab ui elements and set to variables
+const UIguestForm = document.querySelector('#guest-form');
+const UIguestList = docuemnt.querySelector('collection');
+const UIclearBtn = document.querySelector('.clear-guests');
+const UIfilter = document.querySelector('#filter');
+const taskInput = document.querySelector('#guest');
