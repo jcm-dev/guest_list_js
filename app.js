ce0552ba62b48e40ea1cceed96f66c025bec08c6
Joshua <meyer.josh8@gmail.com>
2021-02-22 20:58:09 -0600

    Create addGuest function

--- app.js
+++ app.js
@@ -11,5 +11,38 @@ loadEventListeners();
 // LISTNER CALL FUNCTION
 function loadEventListeners(){
 
+  
 }
 
+// ADD GUEST FUNCTION
+function addGuest(e){
+  // test input box for value
+  if (UIguestInput.value === '') {
+    alert ('Add a guest');
+  }
+
+  // create empty <li>
+  const li = document.createElement('li');
+  // assign empty <li> the collection-item class
+  li.className = 'collection-item';
+  // create and insert text node to empty <li>
+  // with the value of user input
+  li.appendChild(document.createTextNode(UIguestInput.value));
+  // create empty <a>
+  const link = document.createElement('a');
+  // assign empty <a> the delete-item and secondary-content class
+  link.className = 'delete-item secondary-content';
+  // insert <i> tag to empty link and assign fontawesome icon classes
+  link.innerHTML = '<i class="fa fa-remove"></i>';
+  // append the link to the empty <li>
+  li.appendChild(link);
+  // append the newly appeneded <li> to the <ul>
+  UIguestList.appendChild(li);
+
+
+
+  // clear input box after each entry
+  UIguestInput.value = '';
+
+  e.preventDefault();
+}
